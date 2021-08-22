4ee9b07a577d262e557cf74b5d08ab59a83bf3a5
Andrew Bueide <andrew@abueide.com>
2021-08-22 11:22:18 -0500

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    kotlin("multiplatform") version "1.5.10"
+}
+
+group = "org.chiachat.kchia"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnit()
+        }
+    }
+    js(IR) {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+    iosArm64 {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    iosX64 {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+        val iosArm64Main by getting
+        val iosArm64Test by getting
+        val iosX64Main by getting
+        val iosX64Test by getting
+    }
+}
